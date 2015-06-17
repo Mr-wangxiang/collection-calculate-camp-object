74934eec70940a0a181fde36baf2b0fdc46aa837
Mr-wangxiang <wxthoughtworks@outlook.com>
2015-06-17 21:35:59 +0000

    To the first practices/reduce/find_first_even.js

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -1,10 +1,8 @@
 'use strict';
 var _ = require('../../mylodash/array.js');
 function find_first_even(collection) {
-
-    var first_even = _(collection).filter(function (element) {
+    return _(collection).first(function (element) {
         return element%2 === 0;
-    }).first() ;
-    return first_even;
+    });
 }
 module.exports = find_first_even;
